e53a755210364ce405dab46c9a315968d1d71ee6
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-06-19 21:38:52 +0330

    Validator class: added methods to validate inputs and operator WarningPrinter class: the class added to include methods to print related errors.

--- Calculator/Validator.cs
+++ Calculator/Validator.cs
@@ -1,7 +1,33 @@
-﻿namespace Calculator
+﻿using System.Globalization;
+
+namespace Calculator
 {
     // this class validates the inputs and operator
     internal class Validator
     {
+        
+        public bool IsInputValid(string input)
+        {
+            if (double.TryParse(input, out _))
+                return true;
+
+            return false;
+        }
+
+
+        public bool IsOperatorValid(string op)
+        {
+            switch(op)
+            {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    return true;
+                default:
+                    return false;
+
+            }
+        }
     }
 }
